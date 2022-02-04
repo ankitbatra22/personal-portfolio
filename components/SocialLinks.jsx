2b97ce92db059b0c349bf6290517da6438f1dffd
Ankit Batra <56373187+ankitbatra22@users.noreply.github.com>
2022-02-04 17:39:14 -0500

    main page done

---
+++ components/SocialLinks.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+import { Button } from "reactstrap";
+
+import { socialLinks } from "../portfolio";
+
+const SocialLinks = () => {
+	return (
+		<div className="btn-wrapper text-lg">
+
+			<Button
+				className="btn-icon-only rounded-circle ml-1"
+				color="github"
+				rel="noopener"
+				aria-label="Linkedin"
+				href={socialLinks.linkedin}
+				target="_blank"
+			>
+				<span className="btn-inner--icon">
+					<i className="fa fa-linkedin" />
+				</span>
+			</Button>
+
+			<Button
+				className="btn-icon-only rounded-circle ml-1"
+				color="github"
+				href={socialLinks.github}
+				rel="noopener"
+				aria-label="Github"
+				target="_blank"
+			>
+				<span className="btn-inner--icon">
+					<i className="fa fa-github" />
+				</span>
+			</Button>
+
+			<Button
+				className="btn-icon-only rounded-circle ml-1"
+				color="github"
+				href={socialLinks.email}
+				rel="noopener"
+				aria-label="Github"
+				target="_blank"
+			>
+				<span className="btn-inner--icon">
+					<i className="fa fa-envelope" />
+				</span>
+			</Button>
+
+			<Button
+				className="btn-icon-only rounded-circle ml-1"
+				color="github"
+				href={socialLinks.twitter}
+				rel="noopener"
+				aria-label="Twitter"
+				target="_blank"
+			>
+				<span className="btn-inner--icon">
+					<i className="fa fa-twitter" />
+				</span>
+			</Button>
+		</div>
+	);
+};
+
+export default SocialLinks;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
+import dynamic from "next/dynamic";
+import PropTypes from "prop-types";
+const Greetings = dynamic(() => import("../containers/Greetings"));
+const Navigation = dynamic(() => import("../components/Navigation"));
+
 export default function Home() {
 	return (
-		<p>Hello World!</p>
+    <div>
+      <Navigation />
+      <Greetings />
+    </div>
+    
 	);
 }
 
