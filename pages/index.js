3c1c45f290ece26ff9b688ae6fdb91ada1b46587
Ankit Batra <56373187+ankitbatra22@users.noreply.github.com>
2022-02-05 02:06:55 -0500

    skills section:

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import dynamic from "next/dynamic";
 import PropTypes from "prop-types";
+import Proficiency from "../containers/Proficiency";
 const Greetings = dynamic(() => import("../containers/Greetings"));
 const Navigation = dynamic(() => import("../components/Navigation"));
+const Skills = dynamic(() => import("../containers/Skills"));
 
 export default function Home() {
 	return (
     <div>
       <Navigation />
       <Greetings />
+      <Skills />
+      <Proficiency />
     </div>
     
 	);
