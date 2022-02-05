69d68999c43abac5ea5a1cd40ea4a9eecb699a99
Ankit Batra <56373187+ankitbatra22@users.noreply.github.com>
2022-02-05 02:31:58 -0500

    formatting

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,28 @@ const Greetings = dynamic(() => import("../containers/Greetings"));
 const Navigation = dynamic(() => import("../components/Navigation"));
 const Skills = dynamic(() => import("../containers/Skills"));
 
+import { openSource } from "../portfolio";
+import SEO from "../components/SEO";
+
+const keywordsList = [
+	"Ankit","Ankit Batra", "ankitbatra22", "Ankit Batra Portfolio", "Ankit Portfolio ", "Ankit Batra ML", "machine learning",
+	"full stack", "Software Engineer", "Machine Learning Engineer", "Computer Vision", "Flask",
+	"University of Waterloo", "ankitbatra", "pytorch ", "tensorflow", "reactjs ", "opencv", "mechatronics", "waterloo",
+]
+
 export default function Home() {
 	return (
     <div>
+			<SEO
+				data={{
+					title: "Ankit Batra",
+					description:
+						"A passionate Mechatronics engineering student @UWaterloo with an interest in Deep Learning, Computer Vision, Backend & Cloud Computing.",
+					image: `https://api.github.com/users/${openSource.githubUserName}`,
+					url: "",
+					keywords: keywordsList,
+				}}
+			/>
       <Navigation />
       <Greetings />
       <Skills />
